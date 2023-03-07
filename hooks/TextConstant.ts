64de39d5f37d9fc009b7fcfbb20aec497b8f59a3
JaemoKoo <devgoozz@gmail.com>
2023-03-07 21:50:41 +0900

    feat: 각 화면에서 사용 할 텍스트 파일 추가

---
+++ hooks/TextConstant.ts
@@ -0,0 +1,13 @@
+export const INTRO_PAGE = {
+  LEFT_TOP_TEXT: "안녕하세요! 🙋🏻‍♂️",
+  LEFT_MID_TEXT: "Front-end Developer",
+  LEFT_BOTTOM_TEXT: `구재모 입니다! :) `,
+};
+
+export const ABOUT_PAGE = {
+  ABOUT_TEXT: "",
+};
+
+export const PROJECT_PAGE = {};
+
+export const CAREER_PAGE = {};
