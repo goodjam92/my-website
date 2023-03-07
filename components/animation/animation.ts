63cee928fef3c3512375b8c909d2c30cfe4d9bb5
JaemoKoo <devgoozz@gmail.com>
2023-03-07 21:44:54 +0900

    feat: 공용으로 사용 되는 요소 및 애니메이션 추가

---
+++ components/animation/animation.ts
@@ -0,0 +1,39 @@
+import { keyframes } from "styled-components";
+
+export const fadeInFromLeft = keyframes`
+  0%{
+    transform: translateX(-30rem);
+    opacity: 0;
+    }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+export const fadeOut = keyframes`
+  0%{
+    opacity: 1;
+  }
+  100%{
+    opacity: 0;
+  }
+`;
+
+export const slideIn = keyframes`
+0%{
+    transform: translateX(50rem) scale(.5);
+}
+100%{
+    transform: translateX(0px) scale(1);
+}
+`;
+
+export const slideUp = keyframes`
+0%{
+    transform: translateY(30rem);
+}
+100%{
+    transform: translateY(0px);
+}
+`;
---
+++ components/common/commonStyle.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-top: 11.2rem;
+  padding-bottom: 11.2rem;
+  box-sizing: border-box;
+`;
+
+export const InnerContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ContentBox = styled.div`
+  width: 120rem;
+  height: 100%;
+  margin-left: 10rem;
+  box-sizing: border-box;
+  padding: 4rem;
+`;
