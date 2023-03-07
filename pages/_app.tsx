028cc359fd52b0b3198a6c91f18630668612e3be
JaemoKoo <devgoozz@gmail.com>
2023-03-07 21:47:37 +0900

    style: import 선언 위치 및 필요없는 코드 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,9 @@
-import Header from "@/components/Header";
-import NavButton from "@/components/NavButton";
-import { GlobalStyle } from "@/styles/globals-style";
-import { theme } from "@/styles/theme";
 import type { AppProps } from "next/app";
-import { FullPage } from "react-full-page";
-import styled, { ThemeProvider } from "styled-components";
-
-const WebWrap = styled.div`
-  position: relative;
-  box-sizing: border-box;
-  display: flex;
-`;
+import { ThemeProvider } from "styled-components";
+import { theme } from "@/styles/theme";
+import Header from "@/components/Header";
+import { GlobalStyle } from "@/styles/globals-style";
+import "../public/fonts/font.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
