a58f768455c5182196c0eca275e6a873940ac07e
JaemoKoo <devgoozz@gmail.com>
2023-03-08 20:50:41 +0900

    feat: 현재 페이지가 about페이지 인지 확인해서 State에 저장하도록 하는 함수 작성

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useRef, useState } from "react";
 import { ContentBox } from "@/components/common/commonStyle";
 import MySkils from "@/components/about/MySkils";
 import styled from "styled-components";
 
 export default function About() {
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const [localVisible, setLocalVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (!entry.isIntersecting) {
+          setLocalVisible(false);
+          return;
+        }
+        setLocalVisible(entry.isIntersecting);
+      },
+      {
+        rootMargin: "0px",
+        threshold: 0.2,
+      }
+    );
+    if (aboutRef.current) {
+      observer.observe(aboutRef.current);
+      return;
+    }
+    return () => {
+      if (aboutRef.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(aboutRef.current);
+      }
+    };
+  }, [aboutRef]);
+
   return (
-    <AboutWrap>
+    <AboutWrap ref={aboutRef}>
       <ContentBox>
         <RowContent>
           <LeftContent>
