dd83111ca99f5348ca77b010de5eb735825279c0
JaemoKoo <devgoozz@gmail.com>
2023-03-10 23:06:28 +0900

    fix: 첫번째 프로젝트 내용 const로 작성

--- hooks/TextConstant.ts
+++ hooks/TextConstant.ts
@@ -1,13 +1,26 @@
+import { ProjectProps } from "@/model/ProjectProps";
+
 export const INTRO_PAGE = {
   LEFT_TOP_TEXT: "안녕하세요! 🙋🏻‍♂️",
   LEFT_MID_TEXT: "Front-end Developer",
   LEFT_BOTTOM_TEXT: `구재모 입니다! :) `,
 };
 
-export const ABOUT_PAGE = {
-  ABOUT_TEXT: "",
+export const WBS_PROJECT_TEXT: ProjectProps = {
+  TITLE: "WHICH BEACH SEARCH",
+  PERIOD: "2022. 08 ~ 2022. 10",
+  SIMPLE: "국내 해수욕장 날씨 및 정보 조회 서비스",
+  DETAIL: [
+    `- 모바일 사이즈로 제작 된 전국 모든 해수욕장의 날씨와 정보 조회 서비스 웹입니다.`,
+    `- 휴가 기간이나 해양 레저를 즐기고자 해수욕장을 방문 할 때 좀 더 정확한 날씨 정보를 시간 단위로 확인할 수 있습니다.`,
+    `- 잘 알려지지 않았던 해수욕장에 대한 관심도 이끌어 낼 수 있습니다.`,
+  ],
+  USE_SKILL: [
+    "HTML5",
+    "CSS",
+    "Javascript",
+    "Firebase",
+    "Axios",
+    "RESTfull API",
+  ],
 };
-
-export const PROJECT_PAGE = {};
-
-export const CAREER_PAGE = {};
---
+++ model/ProjectProps.ts
@@ -0,0 +1,7 @@
+export interface ProjectProps {
+  TITLE: string;
+  PERIOD: string;
+  SIMPLE: string;
+  DETAIL: Array<string>;
+  USE_SKILL: Array<string>;
+}
