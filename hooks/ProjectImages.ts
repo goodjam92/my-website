dc1dcf0cdf04f546b96b83531fe5a99f39d3f640
JaemoKoo <devgoozz@gmail.com>
2023-03-10 17:07:20 +0900

    feat: whichbeachsearch 사진 추가

---
+++ hooks/ProjectImages.ts
@@ -0,0 +1,9 @@
+export const WHICH_BEACH_SEARCH = [
+  "/assets/image/whichbeachsearch/main1.png",
+  "/assets/image/whichbeachsearch/main2.png",
+  "/assets/image/whichbeachsearch/bookmark.png",
+  "/assets/image/whichbeachsearch/map.png",
+  "/assets/image/whichbeachsearch/more.png",
+  "/assets/image/whichbeachsearch/signIn.png",
+  "/assets/image/whichbeachsearch/signUp.png",
+];
