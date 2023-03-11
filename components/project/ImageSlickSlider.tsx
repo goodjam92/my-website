f5c8c6911ed49b95f45929b7aed05175266a7f66
JaemoKoo <devgoozz@gmail.com>
2023-03-11 13:57:13 +0900

    style: 사용하지 않는 코드 삭제

--- components/project/ImageSlickSlider.tsx
+++ components/project/ImageSlickSlider.tsx
@@ -44,34 +44,3 @@ const StyledSlider = styled(Slider)`
     box-sizing: border-box;
   }
 `;
-
-const NextButton = styled.div`
-  height: 4rem;
-  width: 4rem;
-  position: absolute;
-  right: -1.2rem;
-  top: 50%;
-  z-index: 100;
-  text-align: right;
-  line-height: 2rem;
-`;
-
-const ButtonIcon = styled.img`
-  height: 100%;
-  width: 100%;
-  margin: 0;
-  color: white;
-  filter: opacity(0.5) drop-shadow(0 0 0 #ffffff);
-  cursor: pointer;
-`;
-
-const PrevButton = styled.div`
-  height: 4rem;
-  width: 4rem;
-  position: absolute;
-  left: -1.2rem;
-  top: 50%;
-  z-index: 50;
-  text-align: left;
-  line-height: 2rem;
-`;
