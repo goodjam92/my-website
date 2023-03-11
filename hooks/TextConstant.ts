4bf26209ddaf9730ae6426e48960da20ec4104cd
JaemoKoo <devgoozz@gmail.com>
2023-03-11 16:41:55 +0900

    fix: 프로젝트 내용 추가 및 변경

--- hooks/TextConstant.ts
+++ hooks/TextConstant.ts
@@ -12,7 +12,7 @@ export const WBS_PROJECT_TEXT: ProjectProps = {
   SIMPLE: "국내 해수욕장 날씨 및 정보 조회 서비스",
   DETAIL: [
     `- 모바일 사이즈로 제작 된 전국 모든 해수욕장의 날씨와 정보 조회 서비스 웹입니다.`,
-    `- 휴가 기간이나 해양 레저를 즐기고자 해수욕장을 방문 할 때 좀 더 정확한 날씨 정보를 시간 단위로 확인할 수 있습니다.`,
+    `- 휴가 기간이나 해양 레저를 즐기고자 해수욕장을 방문 할 때 좀 더 정확한 날씨 및 해상 정보를 시간 단위로 확인할 수 있습니다.`,
     `- 잘 알려지지 않았던 해수욕장에 대한 관심도 이끌어 낼 수 있습니다.`,
   ],
   USE_SKILL: [
@@ -23,4 +23,13 @@ export const WBS_PROJECT_TEXT: ProjectProps = {
     "Axios",
     "RESTfull API",
   ],
+  GITHUB: {
+    repasitory: "DoItStory Repasitory",
+    url: "https://github.com/DoItStory/WhichBeachSearch",
+  },
+  FOOTER: {
+    refType: "Demo",
+    url: "https://whichbeachsearch.web.app",
+  },
+};
 };
