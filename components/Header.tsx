b3814e86bafc3e7a172923e15fe16c5981a4aa45
JaemoKoo <devgoozz@gmail.com>
2023-03-11 13:56:55 +0900

    feat: meta description 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,16 @@ import Head from "next/head";
 export default function Header() {
   return (
     <Head>
-      <title>Jaemo Koo</title>
+      <meta
+        name="description"
+        content="Wellcome to my website, I`m FrontEnd Dveloper Jaemo Koo!!"
+      ></meta>
+      <meta
+        name="description"
+        content="저의 포트폴리오 웹 사이트에 오신 것을 환영합니다! 저는 프론트엔드 개발자 구재모라고 합니다. 잘 부탁드립니다!"
+      ></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Jaemo Koo</title>
     </Head>
   );
 }
