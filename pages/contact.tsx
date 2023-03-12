4321e4904037d8d6d9950974fb0e3da83977ff66
JaemoKoo <devgoozz@gmail.com>
2023-03-12 21:13:15 +0900

    fix: contact 기본 화면 구성

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,22 @@
 import styled from "styled-components";
+import { ContentBox, FlexRowBox } from "@/components/common/commonStyle";
+import ContactRightSection from "@/components/contact/ContactRightSection";
+import ContactLeftSection from "@/components/contact/ContactLeftSection";
+import { backgroundAnimate } from "@/components/animation/animation";
 
 export default function Contact() {
   return (
     <ContactWrap>
-      <ContactContent>Contact Page</ContactContent>
+      <ContentBox>
+        <FlexRowBox>
+          <ContactLeftContent>
+            <ContactLeftSection />
+          </ContactLeftContent>
+          <ContactRightContent>
+            <ContactRightSection />
+          </ContactRightContent>
+        </FlexRowBox>
+      </ContentBox>
     </ContactWrap>
   );
 }
@@ -12,13 +25,22 @@ const ContactWrap = styled.section`
   width: 100%;
   min-height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: #4336d3;
+  background: linear-gradient(-45deg, #303030, #454545, #36363a, #343536);
+  background-size: 400% 400%;
+  animation: ${backgroundAnimate} 20s ease infinite;
 `;
 
-const ContactContent = styled.div`
-  font-size: 10rem;
-  color: white;
-  text-align: center;
+const ContactLeftContent = styled.div`
+  width: 70%;
+  height: 100%;
+  background-color: red;
+`;
+
+const ContactRightContent = styled.div`
+  margin-left: 4rem;
+  min-width: 36rem;
+  height: 100%;
 `;
