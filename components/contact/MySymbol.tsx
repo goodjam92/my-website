d765249c8b28920a1405c0ea604c4a49eaf97e5f
JaemoKoo <devgoozz@gmail.com>
2023-03-13 19:59:44 +0900

    fix: 3D Symbol logo box 컴포넌트 수정

--- components/contact/MySymbol.tsx
+++ components/contact/MySymbol.tsx
@@ -1,20 +1,25 @@
-import { useTexture } from "@react-three/drei";
 import { useRef } from "react";
 import { useFrame } from "react-three-fiber";
 import { TextureLoader } from "three";
+import { RoundedBoxGeometry } from "three-stdlib";
 
-export default function MyGithub(props: any) {
+export default function MySymbol(props: any) {
   const mesh = useRef<any>(null);
   const base = new TextureLoader().load("/assets/image/github.jpeg");
-
+  const geometry = new RoundedBoxGeometry(12, 12, 12, 6, 2);
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => {
     mesh.current.rotation.y += 0.01;
   });
 
   return (
-    <mesh {...props} ref={mesh} scale={[1, 1, 1]}>
-      <boxBufferGeometry args={[10, 10, 0.15]} />
+    <mesh
+      {...props}
+      ref={mesh}
+      geometry={geometry}
+      scale={[1, 1, 1]}
+      position={[0, 0, -10]}
+    >
       <meshStandardMaterial map={base} />
     </mesh>
   );
