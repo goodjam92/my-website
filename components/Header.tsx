b2a3ef241e294b8067087202df57505435468035
JaemoKoo <devgoozz@gmail.com>
2023-03-19 17:44:30 +0900

    fix: 웹사이트 문구 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,10 +9,10 @@ export default function Header() {
       ></meta>
       <meta
         name="description"
-        content="저의 포트폴리오 웹 사이트에 오신 것을 환영합니다! 저는 프론트엔드 개발자 구재모라고 합니다. 잘 부탁드립니다!"
+        content="저의 포트폴리오에 오신 것을 환영합니다! 저는 프론트엔드 개발자 구재모라고 합니다. 잘 부탁드립니다!"
       ></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Jaemo Koo</title>
+      <title>구재모 포트폴리오</title>
     </Head>
   );
 }
